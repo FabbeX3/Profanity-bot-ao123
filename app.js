ba6d39e834b418712fd7d1938cfa0345b7e5fb79
FabbeX3 <ha.mannen@gmail.com>
2018-03-13 18:37:14 +0100

    Profanity bot

---
+++ app.js
@@ -0,0 +1,36 @@
+// Basic stuff
+const Discord = require("discord.js");
+const bot = new Discord.Client()
+const profanities = require("profanities");
+
+// Login
+bot.login(process.env.BOT_TOKEN);
+
+// Listening
+bot.on("message", message =>
+{
+
+    // Variables
+    var sender = message.author;
+    var msg = message.content.toUpperCase();
+    var prefix = "!";
+
+    if(sender.id === "422841374769938433")
+    {
+        return;
+    }
+    // Profanity filter test
+    for(x = 0; x < profanities.length; x++)
+    {
+        if(message.content.toLowerCase() == profanities[x].toLowerCase())
+      {
+          const embed =  new Discord.RichEmbed()
+          .setTitle("Warning!!!")
+          .setAuthor("Profanity Bot")
+          .setColor(16711680)
+          .addField(`@${message.author.username} said something suspicious in: #${message.channel.name}`, `The message was: ${message.content}`)
+          .setTimestamp()
+          bot.channels.get("422852036854546432").send({embed});
+      }
+    }
+});
